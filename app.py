45be77c136a251667ca15c2a83772c234249bda4
Pavitra Dharanitha <pavitradharanitha@gmail.com>
2026-05-21 22:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,24 +6,18 @@ import numpy as np
 with open('house_price_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-st.title("House Price Prediction App")
+# Title
+st.title("🏠 House Price Prediction App")
 
-st.write("Enter house details below")
+st.write("Enter number of rooms")
 
-# Inputs
-area = st.number_input("Area (sq ft)", min_value=0)
+# Input
+rooms = st.number_input("Rooms", min_value=1)
 
-bedrooms = st.number_input("Bedrooms", min_value=0)
-
-bathrooms = st.number_input("Bathrooms", min_value=0)
-
-stories = st.number_input("Stories", min_value=0)
-
-parking = st.number_input("Parking", min_value=0)
-
+# Prediction
 if st.button("Predict Price"):
 
-    features = np.array([[area, bedrooms, bathrooms, stories, parking]])
+    features = np.array([[rooms]])
 
     prediction = model.predict(features)
 
